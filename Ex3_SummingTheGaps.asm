8c754afaa97ef9231fcf78677cf6d368c335fa46
YourMistake <erik.vanlandingham@myemail.indwes.edu>
2015-10-07 10:45:14 -0400

    Chapter 4 Work
    Tentative work for chapter 4, I have yet to test it in visual studio- I
    write my code in notepad++ first :)

--- Ex3_SummingTheGaps.asm
+++ Ex3_SummingTheGaps.asm
@@ -8,12 +8,22 @@ ExitProcess proto,dwExitCode:dword
 .data
 dwarray dword  0,2,5,9,10
 count EQU (LENGTHOF dwarray)
+total dword 0
 
 .code
 main proc
-	
-	;Your code goes here
-	
+	mov, esi OFFSET dwarray
+    
+    mov ecx, count
+	L1:
+        mov eax, [esi]  ;move the first val into eax register
+        inc esi         ;inc to get the next value, and also for the next loop
+        mov ebx, [esi]  ;move that value into ebx register
+        
+        sub ebx, eax    ;get the difference of the two values
+        add total, ebx  ;add the difference to the running total      
+        Loop L1
+        
 	invoke ExitProcess,0
 main endp
 end main
--- Ex7_CopyStringReverse.asm
+++ Ex7_CopyStringReverse.asm
@@ -16,6 +16,14 @@ main PROC
 ; Point EDI to the beginning of the target string:
 ; We do not copy the null terminator byte.
 
+	mov  esi,SIZEOF source	; index register set to the end?
+	mov  ecx,SIZEOF source	; loop counter
+L1:
+	mov  al,source[esi]		; get a character from source
+	mov  target[esi],al		; store it in the target
+	dec  esi				; move to the previous character in the array
+	loop L1					; repeat for entire string
+
 ; Your code goes here	
 
 	mov	BYTE PTR [edi],0			; add a null byte to the target
